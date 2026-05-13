7466a550e738bf38a3c03403f03cb1c69a0c404d
Ryanico <138705704+Ryanico@users.noreply.github.com>
2026-05-13 16:59:16 +0300

    Add Limit & Aliasing Clause Statement

---
+++ Parks_and_rec/5. Limit & Aliasing clause.sql
@@ -0,0 +1,14 @@
+-- Limit & Aliasing
+
+SELECT * 
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 2, 1
+;
+
+
+SELECT gender, AVG(age) as avg_age
+FROM employee_demographics
+GROUP BY gender
+HAVING avg_age > 40
+;
