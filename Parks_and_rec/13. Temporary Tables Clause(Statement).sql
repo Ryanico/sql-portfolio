51e4e67f7649a1ee937be96a3c7a46a4c6d80c2d
Ryanico <138705704+Ryanico@users.noreply.github.com>
2026-05-17 12:54:27 +0300

    Add Temporary Tables Clause Statement File

---
+++ Parks_and_rec/13. Temporary Tables Clause(Statement).sql
@@ -0,0 +1,23 @@
+-- Temporary Tables
+
+CREATE TEMPORARY TABLE temp_table
+(first_name varchar(50),
+last_name varchar(50),
+favorite_movie varchar(100)
+);
+
+INSERT INTO temp_table
+VALUES("Post", "Malon", "Sex in The City");
+
+SELECT *
+FROM temp_table;
+
+
+
+CREATE TEMPORARY TABLE salary_over_50k
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+SELECT *
+FROM salary_over_50k;
